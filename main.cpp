e08daf0a613ceb778dc14f6e155736f28c5ff559
creatorSeraph <dbgksdl00@gmail.com>
2020-11-25 11:11:44 +0900

    * 윈도우 창 생성 코드 추가

--- main.cpp
+++ main.cpp
@@ -1,12 +1,78 @@
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+ATOM MyRegisterClass(HINSTANCE hInstance);
+
+constexpr WCHAR WinClassName[256] = L"multi thread";
+
 int APIENTRY wWinMain(
     _In_ HINSTANCE hInstance,
     _In_opt_ HINSTANCE hPrevInstance,
     _In_ LPWSTR lpCmdLine,
     _In_ int nShowCmd)
 {
+    MyRegisterClass(hInstance);
 
+    DWORD dwStyle = WS_CAPTION | WS_THICKFRAME | WS_MINIMIZEBOX | WS_SYSMENU;
+    auto hWnd = CreateWindowExW(0, WinClassName, L"title", dwStyle,
+        CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+
+    if (!hWnd)
+        return false;
+
+    ShowWindow(hWnd, nShowCmd);
+    UpdateWindow(hWnd);
+
+    MSG msg;
+    ZeroMemory(&msg, sizeof(MSG));
+
+    while (msg.message != WM_QUIT)
+    {
+        if (PeekMessage(&msg, nullptr, NULL, NULL, PM_REMOVE))
+        {
+            if (!TranslateAccelerator(hWnd, nullptr, &msg))
+            {
+                TranslateMessage(&msg);
+                DispatchMessage(&msg);
+            }
+        }
+        else
+        {
+
+        }
+    }
+
+    return 0;
+}
+
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSW wcex;
+
+    wcex.style = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc = WndProc;
+    wcex.cbClsExtra = 0;
+    wcex.cbWndExtra = 0;
+    wcex.hInstance = hInstance;
+    wcex.hIcon = NULL;
+    wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wcex.lpszMenuName = NULL;
+    wcex.lpszClassName = WinClassName;
+
+    return RegisterClassW(&wcex);
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
     return 0;
 }
